39091fe174fcce69da2bf52031023c05c9a2313d
mayu-1a <21nn1a0595.mythri@gmail.com>
2024-10-04 12:06:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 provider "aws"{
-    region = "us-east-1"
+    region         = "us-east-1"
+    access_key         =
+var.AWS_ACCESS_KEY_ID
+    secret_key         =
+var.AWS_SECRET_ACCESS_KEY
 }
 resource"aws_instance""example"{
     ami      ="ami-0c55b159cbfafe1f0"
